893ce2ce91d293489cb2bd1730e257ea5ed0389c
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-01-03 11:32:42 +0530

    Upwork project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
 
 function App() {
   return (
-    
+    <section className='w-full h-full bg-white flex flex-col space-y-2'>
+      <Header/>
+    </section>
   );
 }
 
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <nav className="w-full h-28 grid grid-cols-4 bg-transparent p-6">
+           <section><img className="w-28 h-28" src="https://www.upfinance.io/static/media/logo.73ac2c656ed0f28ef385.png"></img></section>
+           <section className="col-span-2"></section>
+           <section className="col-span-1"></section>
+        </nav>
+    );
+}
+
+export default Header;
