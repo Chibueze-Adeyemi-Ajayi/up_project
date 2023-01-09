3ef4b897d0ae945378adb479e0b5b3a06762a299
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-01-09 14:18:16 +0530

    Web 3 dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -3,7 +3,7 @@ import Footer from "../components/footer";
 import Header from "../components/header";
 import WalletInfo from "../components/wallet-info";
 import Mint from "../dialogs/mint";
-// d
+// dash
 const Dashboard = (props) => {
     const [mint_var, mint_func] = useState(false);
     return (
